37a0c1fa7162100db4655b4d487d38e85bddbe6e
Tony Finucci <tony.finucci.etu@univ-lille.fr>
2022-09-01 16:59:02 +0200

    SousMarins ajout

---
+++ src/projet/SousMarins.java
@@ -0,0 +1,15 @@
+package projet;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SousMarins extends Bateau {
+	
+	public SousMarins(int taille, String name){
+		super(taille, name);
+		this.coordonnees = new ArrayList<String>(Arrays.asList(new String[taille]));
+		this.nombre = 2;
+	}
+	
+	
+}
