857c283f706c888944e1dc1b6adbd6d24d4a8eec
Bastien Warnier <bastien.warnier.etu@univ-lille.fr>
2022-09-01 16:43:06 +0200

    ajout Joueur

---
+++ src/projet/Joueur.java
@@ -0,0 +1,23 @@
+package projet;
+
+public class Joueur {
+
+	private String nom;
+	private int nbBateauxEnVie=5;
+
+	public Joueur(String nom){
+		this.nom=nom;
+	}
+
+	public String getNom(){
+		return nom;
+	}
+
+	public int getNbBateauxEnVie(){
+		return nbBateauxEnVie;
+	}
+
+	public void perdBateaux(){
+		nbBateauxEnVie--;
+	}
+}
