1bedab7f1879464d60cacd47ceaa3400be073011
Ylann Gronnier <ylann.gronnier.etu@univ-lille.fr>
2022-09-02 09:25:02 +0200

    ajout SelectionNbJoueur et Entrée Invalide

--- src/projet/Main.java
+++ src/projet/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		new SelectionNbJoueur();
 		Scanner sc = new Scanner(System.in);
 		String nom = sc.nextLine();
 		sc.close();
